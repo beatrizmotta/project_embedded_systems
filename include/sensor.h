87a9a0ca755ef506cee5ce5f40907aef5a2e71cc
Beatriz Motta <mbmbv@ic.ufal.br>
2025-05-22 00:22:05 -0300

    fix: fixefd grammar error on variable name

--- include/sensor.h
+++ include/sensor.h
@@ -1,2 +1,2 @@
-int calc_velocity_kmh(uint32_t time_microsseconds, uint32_t distance_milimeters);
-int teste(void);
+int calc_velocity_kmh(uint32_t time_microsseconds, uint32_t distance_millimeters);
+bool detect_infraction(int velocity_kmh);
