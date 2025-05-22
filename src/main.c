e60152d850cd7a7b794a825af4200e2425ffed60
Beatriz Motta <mbmbv@ic.ufal.br>
2025-05-22 00:22:57 -0300

    feat: added creation of the application threads and infraction evalution based on speed function

--- src/main.c
+++ src/main.c
@@ -2,19 +2,64 @@
 #include "sensor.h"
 
 int main(void) {
-    int number = teste();
-    printk("Hello from Bia e Fred %d!\n", number);
+    printk("Hello from Bia e Fred!\n");
     return 0; 
 }
 
-void sensor_thread(void *, void *, void *) {
+void main_thread(void *, void *, void *) {
     while (1) {
+        // printk("Main thread running");
         k_msleep(1000);
+    }
+}
+
+void sensor_thread(void *, void *, void *) {
+
+    int example_times_in_microsseconds[2] = {110000, 125000};
+    int counter = 0;
+
+    while (1) {
+
+        uint32_t simulation_time = example_times_in_microsseconds[counter];
+        
+        if (counter == 0) { 
+            counter++;
+        } else {
+            counter = 0; 
+        }
+        
+        int velocity = calc_velocity_kmh(simulation_time, CONFIG_RADAR_SENSOR_DISTANCE_MM);
+        bool infracao = detect_infraction(velocity);
+        printk("Velocidade: %d | Infração?: %d\n", velocity, infracao);
+        k_sleep(K_SECONDS(2));
+        // printk("Sensor thread running");
+        // k_msleep(1000);
     }
 }
 
 void display_thread(void *, void *, void *) {
     while (1) {
+        // printk("Display thread running");
         k_msleep(1000);
     }
 }
+
+void network_thread(void *, void *, void *) {
+    while (1) {
+        // printk("Network thread running");
+        k_msleep(1000);
+    }
+}
+
+void camera_thread(void *, void *, void *) {
+    while (1) {
+        // printk("Camera thread running");
+        k_msleep(1000);
+    }
+}
+
+K_THREAD_DEFINE(main_thread_id, 1024, main_thread, NULL, NULL, NULL, 2, 0, 0);
+K_THREAD_DEFINE(sensor_thread_id, 1024, sensor_thread, NULL, NULL, NULL, 3, 0, 0);
+K_THREAD_DEFINE(display_thread_id, 1024, display_thread, NULL, NULL, NULL, 4, 0, 0);
+K_THREAD_DEFINE(network_thread_id, 1024, network_thread, NULL, NULL, NULL, 5, 0, 0);
+K_THREAD_DEFINE(camera_thread_id, 1024, camera_thread, NULL, NULL, NULL, 6, 0, 0);
--- src/sensor.c
+++ src/sensor.c
@@ -1,13 +1,19 @@
 #include <stdint.h>
+#include <stdbool.h>
+#include <zephyr/kernel.h>
 
-#define MICROSECONDS_ON_A_HOUR 3600000000
+#define MICROSECONDS_ON_A_HOUR 3600000000UL
 
-int calc_velocity_kmh(uint32_t time_microsseconds, uint32_t distance_milimeters) {
+int calc_velocity_kmh(uint32_t time_microsseconds, uint32_t distance_millimeters) {
     if (time_microsseconds == 0) return 0; 
-    uint64_t velocity = ((uint64_t) distance_milimeters * MICROSECONDS_ON_A_HOUR) / (time_microsseconds * 1000000); 
-    return (int) velocity; 
+    
+    uint64_t speed = (uint64_t) distance_millimeters * MICROSECONDS_ON_A_HOUR;
+    speed /= (uint64_t) time_microsseconds;
+    speed /= 1000000UL; 
+
+    return (int) speed; 
 }
 
-int teste(void) {
-    return 1; 
+bool detect_infraction(int velocity_kmh) {
+    return velocity_kmh > CONFIG_RADAR_SPEED_LIMIT_KMH;
 }
